0219caad37d9dab78d44702e5388b76c37977791
Alex <alokil70@gmail.com>
2019-11-16 02:18:46 +0300

    Start

---
+++ src/main/java/ru/zakup/zakupshik/ZakupshikApplication.java
@@ -0,0 +1,13 @@
+package ru.zakup.zakupshik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakupshikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakupshikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zakup/zakupshik/ZakupshikApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zakup.zakupshik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZakupshikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
